358a7c9f0205ab38fca1ddbe454c0bced7905a95
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-08-10 00:33:46 +0530

    Create sol_iterative_1stack.java

---
+++ 145. Binary Tree Postorder Traversal/sol_iterative_1stack.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {   
+        List<Integer> l= new ArrayList<>();
+        //postorder-- left, right, root
+        //iterative using 1 stack
+        Stack<TreeNode> st= new Stack<>();
+        if (root == null) return l;
+        TreeNode cur= root;
+        TreeNode temp;
+        while(cur != null || !st.isEmpty()){
+            if(cur!=null){
+                st.push(cur);
+                cur=cur.left;
+            }
+            else{
+                temp=st.peek().right;
+                if(temp==null){
+                    temp=st.pop();
+                    l.add(temp.val);
+                    while(!st.isEmpty() && temp==st.peek().right){
+                        temp=st.pop();
+                        l.add(temp.val);
+                    }
+                }
+                else{
+                    cur=temp;
+                }
+            }
+        }               
+        return l;
+        
+    }
+}
