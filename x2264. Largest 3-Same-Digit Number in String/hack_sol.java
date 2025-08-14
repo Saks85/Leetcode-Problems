71ab3748911a8b28139eb37c231924932212e04d
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-08-14 16:28:24 +0530

    Create hack_sol.java

---
+++ x2264. Largest 3-Same-Digit Number in String/hack_sol.java
@@ -0,0 +1,13 @@
+//another approaoch-- hack method 
+class Solution {
+    public String largestGoodInteger(String num) {
+     String[] numbers={"999","888","777","666","555","444","333","222","111","000"};
+     for(String i:numbers){
+        if(num.contains(i)){
+            return i;
+        }
+     }
+     return "";
+
+    }
+}
