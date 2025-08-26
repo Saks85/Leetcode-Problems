3308d22c84342fc3f35da441a0f4da308e056f01
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-08-26 20:02:27 +0530

    Update dfs_sol.java

--- 547. Number of Provinces/dfs_sol.java
+++ 547. Number of Provinces/dfs_sol.java
@@ -1,29 +1,23 @@
 class Solution {
-    private void dfs(int node, boolean[] vis, List<List<Integer>> adj){
+    private void dfs(int node, boolean[] vis, int[][] adj){
             vis[node]=true;
-            for(Integer it: adj.get(node)){
-                if(vis[it]==false){
-                    dfs(it, vis, adj);
+            int n=adj.length;
+            for(int i=0; i<n; i++){
+                if(vis[i]==false && adj[node][i]==1){
+                    dfs(i, vis, adj);
                 }
             }
     }
     public int findCircleNum(int[][] isConnected) {
         int n= isConnected.length;
         boolean[] vis=new boolean[n];
-        List<List<Integer>> adj= new ArrayList<>();
-        //converting adjacency matrix to adjacency list
-        for(int i=0; i<n; i++){
-            adj.add(new ArrayList<>());
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j]==1) adj.get(i).add(j);                
-            }
-        }
+        
         int count=0;
         for(int i=0; i<n; i++){
             if(!vis[i]){
                 //vis[i]=true;
                 count++;
-                dfs(i,vis,adj);
+                dfs(i,vis,isConnected);
             }
 
         }
