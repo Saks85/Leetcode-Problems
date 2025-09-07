d0ef81acea34e7ea607f343040e43966e808b163
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-09-07 11:48:43 +0530

    Create sol.java

---
+++ x1304. Find N Unique Integers Sum up to Zero/sol.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans= new int[n];
+        int j=0;
+        for (int i=0; i<n/2; i++){
+            ans[j++]=i+1;
+            ans[j++]=-i-1;
+        }
+        return ans;
+    }
+}
