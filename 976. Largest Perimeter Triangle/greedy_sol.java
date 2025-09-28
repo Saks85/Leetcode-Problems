366b0c2ca13f400cd2ded8a90eb749689e812b1f
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-09-28 13:10:25 +0530

    Create greedy_sol.java

---
+++ 976. Largest Perimeter Triangle/greedy_sol.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+
+        Arrays.sort(nums); // Sort the array
+
+        // Start from the largest side
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0; // No valid triangle
+    }
+}
