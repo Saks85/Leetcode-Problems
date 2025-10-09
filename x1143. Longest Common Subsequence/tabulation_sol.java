02fb7ea8b1b8a8342cb9a40b35a58777e953ddb1
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-10-10 00:37:40 +0530

    Update tabulation_sol.java

--- x1143. Longest Common Subsequence/tabulation_sol.java
+++ x1143. Longest Common Subsequence/tabulation_sol.java
@@ -1,15 +1,4 @@
 class Solution {
-    private int f(int i1, int i2, String s1, String s2, int[][] dp) {
-        if (i1 < 0 || i2 < 0)
-            return 0;
-        if (dp[i1][i2] != -1)
-            return dp[i1][i2];
-
-        if (s1.charAt(i1) == s2.charAt(i2))
-            return dp[i1][i2] = 1 + f(i1 - 1, i2 - 1, s1, s2, dp);
-        return dp[i1][i2] = Math.max(f(i1 - 1, i2, s1, s2, dp), f(i1, i2 - 1, s1, s2, dp));
-
-    }
 
     public int longestCommonSubsequence(String s1, String s2) {
         int n=s1.length();
