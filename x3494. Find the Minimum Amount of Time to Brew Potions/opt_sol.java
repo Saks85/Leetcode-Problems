dc39dcb267fe8d16d49b0c56687b42afe169fa9b
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-10-09 23:16:50 +0530

    beats 81%

---
+++ x3494. Find the Minimum Amount of Time to Brew Potions/opt_sol.java
@@ -0,0 +1,15 @@
+class Solution {
+    long minTime(int[] skill, int[] mana) {
+    long[] endTime = new long[skill.length];
+    for(int i = 0; i < mana.length; ++i){
+        long t = 0, maxDiff = 0;
+        for(int j = 0; j < skill.length; ++j){
+            maxDiff = Math.max(maxDiff, endTime[j] - t);
+            t += (long)skill[j] * (long)mana[i];
+            endTime[j] = t;
+        }
+        for(int j = 0; j < skill.length; ++j) endTime[j] += maxDiff;
+    }
+    return endTime[endTime.length-1];
+}
+}
