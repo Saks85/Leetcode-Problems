47f2903c8780a9b3802ab2008e55a4285bbba341
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-10-21 23:20:03 +0530

    Create memo_sol.java

---
+++ 123. Best Time to Buy and Sell Stock III/memo_sol.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int f(int ind, int buy, int cap, int[] prices, int[][][] dp) {
+        if (ind == prices.length || cap == 0) return 0;
+        if (dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+        int profit;
+        if (buy == 1) {
+            profit = Math.max(
+                -prices[ind] + f(ind + 1, 0, cap, prices, dp),
+                f(ind + 1, 1, cap, prices, dp)
+            );
+        } else {
+            profit = Math.max(
+                prices[ind] + f(ind + 1, 1, cap - 1, prices, dp),
+                f(ind + 1, 0, cap, prices, dp)
+            );
+        }
+        return dp[ind][buy][cap] = profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][2][3]; // 3 caps: 0, 1, 2
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return f(0, 1, 2, prices, dp);
+    }
+}
