4b04257dd1de89d228e8d24f54820e913564ac9f
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-10-22 18:55:16 +0530

    Create tabulation_sol.java

---
+++ 300. Longest Increasing Subsequence/tabulation_sol.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[][] dp= new int[n+1][n+1];
+
+        for(int i=n-1; i>=0; i--){
+            for (int j=i-1; j>=-1; j--){
+                int len= 0+dp[i+1][j+1]; //not take
+                if(j==-1 || nums[i]>nums[j]){
+                    len= Math.max(len,1+dp[i+1][i+1]); //max of not-take and take case
+                }
+                dp[i][j+1]=len;
+            }
+        }
+        return dp[0][0];
+    }
+}
