1bd5fcd6f64f3f1d6c93a81547515377475d1691
Saksham Gupta <156185060+Saks85@users.noreply.github.com>
2025-11-27 23:28:09 +0530

    Create slow_fast_sol.java

---
+++ 287. Find the Duplicate Number/slow_fast_sol.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+
+        // Phase 1: detect cycle
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        // Phase 2: find cycle start (duplicate)
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
